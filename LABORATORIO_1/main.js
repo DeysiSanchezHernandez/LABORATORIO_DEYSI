691fea8d0aa919dabda183056a244d33e44f7397
Deysi Sanchez <7559897842@untrm.edu.pe>
2026-06-18 09:51:34 -0500

    declaración switch donde cada case sea un literal

--- LABORATORIO_1/main.js
+++ LABORATORIO_1/main.js
@@ -35,3 +35,22 @@ if (sabor === "chocolate") {
     console.log("Prefieres otro sabor.");
 }
 
+// PASO 5 - switch
+let opcion = 2;
+
+switch (opcion) {
+    case 1:
+        console.log("Elegiste la opción 1");
+        break;
+
+    case 2:
+        console.log("Elegiste la opción 2");
+        break;
+
+    case 3:
+        console.log("Elegiste la opción 3");
+        break;
+
+    default:
+        console.log("Opción no válida");
+}
