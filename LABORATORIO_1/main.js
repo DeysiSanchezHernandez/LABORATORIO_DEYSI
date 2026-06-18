66011312a01b7c62a8dcdd0bb4d1070d436f6ea2
Deysi Sanchez <7559897842@untrm.edu.pe>
2026-06-18 09:44:53 -0500

    fase 2 typeof

--- LABORATORIO_1/main.js
+++ LABORATORIO_1/main.js
@@ -5,3 +5,9 @@ console.log("Iniciando laboratorio...");
 // FASE 2: Variables y Literales Primitivos 
 let edad = 20;
 let esEstudiante = true;
+
+console.log("Edad:", edad);
+console.log("Tipo de edad:", typeof edad);
+
+console.log("Es estudiante:", esEstudiante);
+console.log("Tipo de esEstudiante:", typeof esEstudiante);
